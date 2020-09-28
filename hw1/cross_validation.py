ff115444f0e50df11c71d03fe233fbd8fc0f0c09
Camelia D. Brumar <cbrumar@umiacs.umd.edu>
2020-09-27 23:09:49 -0400

    implemented the function. needs to be validated

--- hw1/cross_validation.py
+++ hw1/cross_validation.py
@@ -1,7 +1,7 @@
 import numpy as np
 from LeastSquaresLinearRegression import LeastSquaresLinearRegressor # TODO remove this line before submitting and the main function
 
-from performance_metrics import calc_mean_squared_error
+from performance_metrics import calc_mean_squared_error # was this here??
 
 
 def train_models_and_calc_scores_for_n_fold_cv(
@@ -58,10 +58,38 @@ def train_models_and_calc_scores_for_n_fold_cv(
     '''
     train_error_per_fold = np.zeros(2, dtype=np.int32)
     test_error_per_fold = np.zeros(2, dtype=np.int32)
+    
+    #for i in range(n_folds):
 
     # TODO define the folds here by calling your function
     # e.g. ... = make_train_and_test_row_ids_for_n_fold_cv(...)
+    
+    N = x_NF.shape[0]
+    # get the indices for training and testing per fold
+    tr_ids_per_fold, te_ids_per_fold = make_train_and_test_row_ids_for_n_fold_cv(N, n_folds, random_state)
+    
+    for i in range(n_folds):
+        # concatenate in one array all the training points
+        ith_training_xs = list()
+        ith_training_ys = list()
+        for j in range (n_folds - 1):
+            ith_training_xs = np.append(ith_training_points, x_NF[np.array(tr_ids_per_fold[i][j])]) # TODO change the final 0)
+            ith_training_ys = np.append(ith_training_ys, y_N[np.array(tr_ids_per_fold[i][j])])
 
+        # Now we have all the training points, we can fit our model
+        estimator.fit(ith_training_xs, ith_training_ys)
+        # we obtain the predictions for the training points
+        ith_tr_y_hat = estimator.predict(ith_training_xs)
+        # we compute the training error and add it to the train_error_per_fold numpy array
+        train_error_per_fold[i] = calc_mean_squared_error(ith_training_ys, ith_y_hat)
+        
+        # now we compute the testing/cross validation error
+        ith_testing_xs = x_NF[np.array(te_ids_per_fold[i])]
+        ith_testing_ys = y_N[np.array(te_ids_per_fold[i])]
+        
+        ith_te_y_hat = estimator.predict(ith_testing_xs)
+        test_error_per_fold[i] = calc_mean_squared_error(ith_testing_ys, ith_te_y_hat)
+        
     # TODO loop over folds and compute the train and test error
     # for the provided estimator
 
@@ -172,13 +200,13 @@ if __name__ == '__main__': # TODO eliminate the main function
     # With toy dataset with N=100 examples
     # created via a known linear regression model plus small noise
 
-    prng = np.random.RandomState(0)
-    N = 100
+    #prng = np.random.RandomState(0)
+    #N = 100
 
-    true_w_F = np.asarray([1.1, -2.2, 3.3])
-    true_b = 0.0
-    x_NF = prng.randn(N, 3)
-    y_N = true_b + np.dot(x_NF, true_w_F) + 0.03 * prng.randn(N)
+    #true_w_F = np.asarray([1.1, -2.2, 3.3])
+    #true_b = 0.0
+    #x_NF = prng.randn(N, 3)
+    #y_N = true_b + np.dot(x_NF, true_w_F) + 0.03 * prng.randn(N)
 
     #linear_regr = LeastSquaresLinearRegressor()
     #linear_regr.fit(x_NF, y_N)
@@ -198,12 +226,36 @@ if __name__ == '__main__': # TODO eliminate the main function
     #print("shape")
     #print(yhat_N.shape)
     
-    tr_ids_per_fold, te_ids_per_fold = (make_train_and_test_row_ids_for_n_fold_cv(11, 3))
+    #tr_ids_per_fold, te_ids_per_fold = (make_train_and_test_row_ids_for_n_fold_cv(11, 3))
     #print("the size of tr_ids_per_fold is", len(tr_ids_per_fold), "where each element has size ", len(tr_ids_per_fold[0]))
     #print(tr_ids_per_fold)
     #print("<---------------------------->")
     #print("the size of te_ids_per_fold is", len(te_ids_per_fold), "where each element has size ", len(te_ids_per_fold[0]))
     #print(te_ids_per_fold)
+    
+    # Create simple dataset of N examples where y given x
+    # is perfectly explained by a linear regression model
+    N = 101
+    n_folds = 7
+    x_N3 = np.random.RandomState(0).rand(N, 3)
+    y_N = np.dot(x_N3, np.asarray([1., -2.0, 3.0])) - 1.3337
+    
+    arr1 = [[np.asarray([1, 2]), np.asarray([3, 4])], 
+            [np.asarray([5, 6]), np.asarray([7, 8])]]
+    
+    
+    arr2 = np.append(arr1[1][0], arr1[1][1])
+    print(arr2)
+    print(arr1[1][0])
+                                        
+    
+    print(arr2[np.array([3, 3, 1, 2])])
+    train_error_per_fold = np.zeros(2, dtype=np.int32)
+    print(train_error_per_fold)
+    #print(print(train_error_per_fold).shape)
+    train_error_per_fold[0] = 1
+    print(train_error_per_fold)
+    
     
 
 
